68dbbeaf73b60309f1b40cc07712eae152458575
Psychot1X <riezky03@gmail.com>
2025-07-21 17:49:24 +0700

    fix admin

--- src/app/admin/_components/add-movie-form.jsx
+++ src/app/admin/_components/add-movie-form.jsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useFormState, useFormStatus } from "react-dom";
-import { useEffect, useRef } from "react";
+import { useActionState, useEffect, useRef } from "react";
+import { useFormStatus } from "react-dom";
 import { PlusCircle } from "lucide-react";
 import { toast } from "sonner";
 
@@ -31,7 +31,7 @@ function SubmitButton() {
 }
 
 export function AddMovieForm() {
-  const [state, formAction] = useFormState(addMovie, { message: null });
+  const [state, formAction] = useActionState(addMovie, { message: null });
   const formRef = useRef(null);
 
   useEffect(() => {
